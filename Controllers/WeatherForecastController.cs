373363196e1430122ddeedca90d1e73f860fe819
Jean Parpaillon <jean.parpaillon@free.fr>
2024-12-19 12:41:36 +0100

    fix issue

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,11 +11,10 @@ namespace API_Dotnet_Test.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        // private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> _logger)
         {
-            _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
